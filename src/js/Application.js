5047bbf5970573cce306f562640797f2b297062f
MariellaNik <118596924+MariellaNik@users.noreply.github.com>
2023-02-04 15:41:39 +0200

    Update Application.js

--- src/js/Application.js
+++ src/js/Application.js
@@ -5,40 +5,26 @@ export default class Application extends EventEmitter {
   static get events() {
     return {
       READY: "ready",
-      BIT: "bit",
     };
   }
 
   constructor() {
     super();
-
     this._beat = new Beat();
-    this._create();
-
-    this._beat.on(Beat.events.BIT, this._createMessage.bind(this));
-
     const lyrics = ["Ah", "ha", "ha", "ha", "stayin' alive", "stayin' alive"];
-    this._lyrics = lyrics;
-    this._count = 0;
+    //this._lyrics = lyrics;
+    let count = 0;
 
+   this._beat.addListener(Beat.events.BIT, () =>
+      this._create(lyrics[count > lyrics.length - 1 ? (count = 0) : count++])
+    );
     this.emit(Application.events.READY);
   }
 
-  _create() {
+  _create(msg) {
     const message = document.createElement("div");
     message.classList.add("message");
-    message.innerText = this._lyrics[this._count];
-
-    document.querySelector(".main").appendChild(message);
-  }
-
-  _createMessage() {
-    this._count = (this._count + 1) % this._lyrics.length;
-
-    const message = document.createElement("div");
-    message.classList.add("message");
-    message.innerText = this._lyrics[this._count];
-
+    message.innerText = msg;
     document.querySelector(".main").appendChild(message);
   }
 }
